aa5ee760d93315d71a99eb2e876be6b4a92d4806
Your Name <your.email@example.com>
2025-01-10 23:46:36 -0800

    first commit

---
+++ frontend/notes-app/src/components/Navbar/Bar.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import ProfileInfo from '../Cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom'
+import SearchBar from '../SearchBar/SearchBar'
+
+const Bar = () => {
+
+
+
+  return (
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-meduim text-black py-2">Notes</h2>   
+
+     
+    </div>
+  )
+}
+
+export default Bar
