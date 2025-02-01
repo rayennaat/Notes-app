ed321c366a03ecc4149c2c208533c3b90bbfaa90
rayennaat <93454687+rayennaat@users.noreply.github.com>
2025-02-01 09:13:53 -0800

    Update SignUp.jsx

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -86,7 +86,7 @@ const SignUp = () => {
 
           <p className="text-sm text-center mt-4">
             Already have an  account?{" "}
-            <Link to="/login" className="font-medium text-primary underline">
+            <Link to="/" className="font-medium text-primary underline">
               Login
             </Link> </p>
         </form>
