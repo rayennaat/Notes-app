d2cba35d02594d351a4c702cd77d186400085b4d
Your Name <your.email@example.com>
2025-01-31 18:54:44 -0800

    return it

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: "http://0.0.0.0:5173",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST", "PUT", "DELETE"], // Allow these methods
         allowedHeaders: ["Content-Type", "Authorization"],
     })
